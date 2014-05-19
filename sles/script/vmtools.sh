d5304c95d09892f46d46b6ccaef7f2f159691b3e
John Hughes <jonuwz@gmail.com>
2014-05-19 23:34:26 +0100

    add cloud-init repositories for qemu

--- sles/script/vmtools.sh
+++ sles/script/vmtools.sh
@@ -21,91 +21,17 @@ elif test -e /dev/vda1 ;then
     zypper ar http://192.168.0.101/repos/SLES-11.2 SLES-11.2
     zypper ar http://192.168.0.101/repos/SLES-11.2-sdk SLES-11.2-sdk
     zypper ar -G http://192.168.0.101/repos/SLES-11.2-custom SLES-11.2-custom
+    zypper ar -G http://download.opensuse.org/repositories/Cloud:/Tools/SLE_11_SP2 Cloud:Tools
+    zypper ar -G http://download.opensuse.org/repositories/Cloud:/OpenStack:/Havana/SLE_11_SP3 Cloud:Openstack:Havana
+    zypper ar -G http://download.opensuse.org/repositories/devel:/languages:/python/SLE_11_SP2 devel:languages:python
+    zypper mr -p 98 Cloud:Openstack:Havana
+    zypper mr -p 97 SLES-11.2-custom
     zypper ref
     zypper -n in cloud-init openstack-heat-cfntools
 
-    # Remove the default cloud.cfg configuration file
-    rm /etc/cloud/cloud.cfg
-
-    # Create the new cloud.cfg file
-    cat << EOF > /etc/cloud/cloud.cfg
-
-
-# adapted default config for (open)SUSE systems
-user: devopsadmin
-disable_root: False
-preserve_hostname: False
-cc_ready_cmd: [ /bin/true ]
-syslog_fix_perms: root:root
-# datasource_list: ["NoCloud", "ConfigDrive", "OVF", "MAAS", "Ec2", "CloudStack"]
-
-cloud_init_modules:
- - bootcmd
- - growpart
- - resizefs
- - set_hostname
- - update_hostname
- - update_etc_hosts
- - ca-certs
- - rsyslog
- - ssh
-
-cloud_config_modules:
- - disk-setup
- - mounts
- - ssh-import-id
- - locale
- - set-passwords
- - landscape
- - timezone
- - puppet
- - chef
- - salt-minion
- - mcollective
- - disable-ec2-metadata
- - runcmd
-
-cloud_final_modules:
- - rightscale_userdata
- - scripts-per-once
- - scripts-per-boot
- - scripts-per-instance
- - scripts-user
- - keys-to-console
- - final-message
-ssh_genkeytypes: ['rsa', 'dsa']
-EOF
-
-# Stick the correct hostname for the cloud templates
-dd of=/etc/cloud/templates/hosts.tmpl << EOF
-#*
-    This file /etc/cloud/templates/hosts.redhat.tmpl is only utilized
-    if enabled in cloud-config.  Specifically, in order to enable it
-    you need to add the following to config:
-      manage_etc_hosts: True
-*#
-# Your system has configured 'manage_etc_hosts' as True.
-# As a result, if you wish for changes to this file to persist
-# then you will need to either
-# a.) make changes to the master file in /etc/cloud/templates/hosts.redhat.tmpl
-# b.) change or remove the value of 'manage_etc_hosts' in
-#     /etc/cloud/cloud.cfg or cloud-config from user-data
-#
-# The following lines are desirable for IPv4 capable hosts
-127.0.0.1 localhost.localdomain localhost
-127.0.0.1 localhost4.localdomain4 localhost4
-1.2.3.4   \${fqdn} \${hostname}
-
-
-# The following lines are desirable for IPv6 capable hosts
-::1 localhost.localdomain localhost
-::1 localhost6.localdomain6 localhost6
-
-EOF
-
-  chkconfig cloud-init-local on
-  chkconfig cloud-init on
-  chkconfig cloud-config on
-  chkconfig cloud-final on
+    chkconfig cloud-init-local on
+    chkconfig cloud-init on
+    chkconfig cloud-config on
+    chkconfig cloud-final on
 
 fi
